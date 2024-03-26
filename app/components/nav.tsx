34c20758ffedae573d4d03e703665025e81dc30d
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-03-26 00:16:18 -0400

    updates

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -24,20 +24,19 @@ export default function Nav(prop: HamburgerProps) {
   const handleMenu = (e: any ) => {
     e.preventDefault();
     setIsOpen(!isOpen);
-    console.log('being clicked')
 
   }
 
 
 
   return (
-    <div className="flex justify-between w-full absolute bg-transparent z-10 ">
+    <div className="flex justify-between w-full absolute overflow-hidden bg-transparent z-10 ">
     <div className="flex justify-between items-center w-full p-4">
       <Image src={Logo} alt='home page' width={110} height={110}></Image>
       <HiMenuAlt1 size={40} className="md:hidden" color="white" onClick={handleMenu}/>
     </div>
     <AnimatePresence>
-      {isOpen && <motion.div initial={{scale: 0.2, opacity: 0.2, x: "-100%",}} animate={{x: '60%', scaleX: 0.3, rotate: 180}} transition={{duration: 1, ease: 'easeInOut'}}  whileInView={{scale: 1, opacity: 1, x: '0%', scaleX: 1, rotate: 0}} exit={{scale: 0.2, rotate: 180, opacity: 0.3, x: '-100%'}} className=" w-full h-screen fixed bg-slate-800 top-0 left-0 -z-0 flex flex-col gap-9 items-end">
+      {isOpen && <motion.div initial={{scale: 0.2, opacity: 0.2, x: "-100%",}} animate={{x: '60%', scaleX: 0.3, rotate: 180}} transition={{duration: 1, ease: 'backOut'}}  whileInView={{scale: 1, opacity: 1, x: '0%', scaleX: 1, rotate: 0}} exit={{scale: 0.2, rotate: 180, opacity: 0.2, x: '-100%', scaleX: 0.8}} className=" w-[22rem] h-screen rounded-xl fixed bg-slate-800 top-0 left-0 -z-0 flex flex-col gap-9 items-end">
             <div className="flex justify-center items-end p-4">
             <GoX size={43} color="white" onClick={handleMenu}/>
             </div>
