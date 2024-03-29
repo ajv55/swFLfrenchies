4b2c4035094aaffabcc250aa7712f55cd070e633
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-03-29 17:30:32 -0400

    updated the values page

--- app/components/valueComponents/valueHeader.tsx
+++ app/components/valueComponents/valueHeader.tsx
@@ -5,13 +5,14 @@ import {motion} from 'framer-motion'
 
 export default function ValueHeader() {
   return (
-    <div className=' bg-gradient-to-tl from-slate-700 to-slate-400 relative w-full h-screen flex justify-center items-center'>
+    <div className=' bg-gradient-to-tl from-slate-700 to-slate-400 relative w-full h-screen flex flex-col justify-center items-center gap-5'>
         <div className={style.custom}>
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                 <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className={style.f}></path>
             </svg>
         </div>
         <motion.h1 initial={{scale: 3, opacity: 0, y: '-400%', x: '50%' , scaleY: 0.3, rotateZ: 360}} animate={{scaleY: 0.3, rotate: 210, scale: 0.5, y: '50%', rotateZ: 200  }} viewport={{once: true}} transition={{duration: 1.45, ease: 'circInOut'}} whileInView={{scale: 1, opacity: 1, y: '0%', scaleY: 1, rotate: 0, rotateZ: 0, x: '0%'}} className={`${dancing_script.className} text-7xl text-center bg-gradient-to-bl from-purple-600 via-purple-300 to-fuchsia-500 bg-clip-text text-transparent`}>Our Values</motion.h1>
+        <p className='font-light tracking-wide text-center text-white text-lg'>Where Puppy Love Meets Paw-sitive Principles</p>
     </div>
   )
 }
--- app/components/valueComponents/valueInfo.tsx
+++ app/components/valueComponents/valueInfo.tsx
@@ -3,29 +3,53 @@ import React from 'react'
 
 export default function ValueInfo() {
   return (
-    <div className=' mt-20 mb-48 border-2 border-red-600 w-full h-content flex flex-col gap-9 justify-center items-start'>
-        <div className='border w-full  flex flex-col justify-evenly items-center gap-3 h-[31rem] px-2.5 py-3 bg-slate-700 relative '>
-         <span className='text-[10rem] opacity-30 absolute -top-9 left-0 text-white'>1</span>
-          <h1 className='text-3xl font-bold tracking-wider self-end text-white'> Integrity </h1>
-          <p className='text-center text-white tracking-wide font-light'>Integrity is the bedrock of our business. <span className=' bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We operate with honesty, transparency, and ethical conduct in all aspects of our operations, from breeding practices to customer interactions. We believe that trust is earned through integrity, and we are committed to maintaining the trust of our customers and partners at all times.</span> </p>
+    <div className=' mt-20 mb-48 w-full h-content flex flex-col gap-9 justify-center items-center'>
+        <div className=' w-full flex flex-col justify-evenly items-center gap-3 h-[38rem] px-2.5 py-3 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 left-0 text-white'>1</span>
+          <h1 className='text-3xl font-bold tracking-wider self-end text-white underline underline-offset-[20px] pr-5'> Integrity </h1>
+          <p className='text-left text-xl text-white tracking-wide font-light'><span className='font-bold'>Integrity</span> is the bedrock of our business. <span className=' bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We operate with honesty, transparency, and ethical conduct in all aspects of our operations, from breeding practices to customer interactions. We believe that trust is earned through integrity, and we are committed to maintaining the trust of our customers and partners at all times.</span> </p>
         </div>
 
-        <div className='border w-full  flex flex-col justify-evenly items-center h-[31rem] px-2.5 py-3 bg-slate-700 relative '>
-         <span className='text-[10rem] opacity-30 absolute -top-9 right-0 text-white'>2</span>
-          <h1 className='text-3xl font-bold tracking-wider self-start text-white'> Compassion </h1>
-          <p className='text-center text-white tracking-wide font-light'>Compassion is at the heart of everything we do.  We believe in treating all animals with kindness, empathy, and respect. <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>Our commitment to compassion extends to our breeding practices, customer care, and support for animal welfare initiatives. We strive to create a world where every Frenchie Pitbull is valued, loved, and cared for.</span> </p>
+        <div className=' w-full  flex flex-col justify-evenly items-center h-[38rem] px-2.5 py-3 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 right-0 text-white'>2</span>
+          <h1 className='text-3xl font-bold tracking-wider self-start text-white underline underline-offset-[20px] pl-3 '> Compassion </h1>
+          <p className='text-right text-xl text-white tracking-wide font-light'><span className='font-bold'>Compassion is at the heart of everything we do.</span>  We believe in treating all animals with kindness, empathy, and respect. <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>Our commitment to compassion extends to our breeding practices, customer care, and support for animal welfare initiatives. We strive to create a world where every Frenchie Pitbull is valued, loved, and cared for.</span> </p>
         </div>
 
-        <div className='border w-full  flex flex-col justify-evenly items-center gap-3 h-[31rem] px-2.5 py-3 bg-slate-700 relative '>
-         <span className='text-[10rem] opacity-30 absolute -top-9 left-0 text-white'>3</span>
-          <h1 className='text-3xl font-bold tracking-wider self-end text-white'>Quality</h1>
-          <p className='text-center text-white tracking-wide font-light'>Quality is non-negotiable at <span className={`${alex_brush.className} text-4xl bg-gradient-to-bl from-purple-500 via-slate-300 to-fuchsia-500 bg-clip-text text-transparent`}>SWFLFrenchies</span>. <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We are dedicated to upholding the highest standards of excellence in the care, breeding, and upbringing of our Frenchie Pitbull puppies. </span> From health screenings to socialization to ongoing support, we prioritize quality in every aspect of our business to ensure that our puppies are healthy, happy, and well-adjusted.  </p>
+        <div className=' w-full  flex flex-col justify-evenly items-center gap-3 h-[38rem] px-2.5 py-3 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-12 left-0 text-white'>3</span>
+          <h1 className='text-3xl font-bold tracking-wider self-end text-white underline underline-offset-[20px] pr-10'>Quality</h1>
+          <p className='text-left text-xl pt-20 text-white tracking-wide font-light'>Quality is non-negotiable at <span className={`${alex_brush.className} text-4xl bg-gradient-to-bl from-purple-500 via-slate-300 to-fuchsia-500 bg-clip-text text-transparent`}>SWFLFrenchies</span>. <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We are dedicated to upholding the highest standards of excellence in the care, breeding, and upbringing of our Frenchie Pitbull puppies. </span> From health screenings to socialization to ongoing support, <span className='font-bold'>we prioritize quality in every aspect</span> of our business to ensure that our puppies are healthy, happy, and well-adjusted.  </p>
         </div>
 
-        <div className='border w-full  flex flex-col justify-evenly items-center h-[31rem] px-2.5 py-3 bg-slate-700 relative '>
-         <span className='text-[10rem] opacity-30 absolute -top-9 right-0 text-white'>4</span>
-          <h1 className='text-3xl font-bold tracking-wider self-start text-white'> Responsibility </h1>
-          <p className='text-center text-white tracking-wide font-light'>Compassion is at the heart of everything we do.  We believe in treating all animals with kindness, empathy, and respect. <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>Our commitment to compassion extends to our breeding practices, customer care, and support for animal welfare initiatives. We strive to create a world where every Frenchie Pitbull is valued, loved, and cared for.</span> </p>
+        <div className=' w-full  flex flex-col justify-evenly items-center h-[38rem] px-2.5 py-3 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 right-0 text-white'>4</span>
+          <h1 className='text-3xl font-bold tracking-wider self-start text-white underline underline-offset-[20px]'> Responsibility </h1>
+          <p className='text-center text-white tracking-wide font-light'>As responsible breeders, <span className='font-bold'>we recognize the profound impact</span> that our actions have on the lives of our dogs and their future families.<span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'> We are committed to promoting responsible pet ownership, advocating for ethical breeding practices, and ensuring the welfare of our dogs throughout their lives.</span> <span className='font-bold'>We take our responsibility seriously</span> and strive to make a positive difference in the lives of our dogs and the broader community. </p>
+        </div>
+
+        <div className=' w-full  flex flex-col justify-evenly items-center h-[38rem]  bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 left-0 text-white'>5</span>
+          <h1 className='text-3xl font-bold tracking-wider self-end text-white underline underline-offset-[20px] px-2 '>Community</h1>
+          <p className='text-left mt-32 text-white text-lg px-3 tracking-wide font-light'><span className='font-bold'>Community is a vital part</span> of who we are at <span className={`${alex_brush.className} text-4xl bg-gradient-to-bl from-purple-500 via-slate-300 to-fuchsia-500 bg-clip-text text-transparent`}>SWFLFrenchies</span>. We believe in fostering a supportive and inclusive community of Frenchie Pitbull enthusiasts, pet owners, and animal lovers.  <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We celebrate the joy of pet ownership, share knowledge and resources, and offer a platform for meaningful connections between like-minded individuals who share our passion for these amazing dogs.</span> </p>
+        </div>
+
+        <div className=' w-full  flex flex-col justify-evenly items-center h-[38rem] px-2.5 py-3 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 right-0 text-white'>6</span>
+          <h1 className='text-3xl font-bold tracking-wider self-start text-white underline  underline-offset-4'>Continuous <br /> Improvement</h1>
+          <p className='text-center text-white text-lg tracking-wide font-light'>We are committed to continuous improvement and innovation. <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text  text-transparent'>We believe that there is always room to grow and learn, and we are dedicated to evolving and adapting to meet the changing needs of our customers and the broader pet community.</span> <span className='font-bold'>We embrace feedback</span>, challenge the status quo, and strive for excellence in everything we do.</p>
+        </div>
+
+        <div className=' w-full  flex flex-col justify-evenly items-center h-[38rem] px-2.5 py-3 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 left-0 text-white'>7</span>
+          <h1 className='text-3xl font-bold tracking-wider self-end text-white underline underline-offset-[20px]'>Transparency</h1>
+          <p className='text-center text-white text-lg tracking-wide font-light'><span className='font-bold'>Transparency is key </span>to building trust with our customers. We believe in open communication and honesty in all our dealings.  <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We provide transparent information about our breeding practices, health screenings, and the upbringing of our puppies, empowering our customers to make informed decisions with confidence.</span></p>
+        </div>
+
+        <div className=' w-full  flex flex-col justify-evenly items-center h-[38rem] px-2.5 py-2 bg-slate-700 relative '>
+         <span className='text-[12rem] opacity-30 absolute -top-9 right-0 text-white'>8</span>
+          <h1 className='text-3xl font-bold tracking-wider self-start text-white underline underline-offset-[20px]'>Sustainability</h1>
+          <p className='text-center text-lg  text-white tracking-wide font-light'>We are committed to <span className='font-bold'>sustainability</span> and minimizing our environmental impact.  <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'>We strive to operate our business in an environmentally responsible manner, reducing waste, conserving resources, and promoting eco-friendly practices wherever possible. </span><span className='font-bold'> We believe that</span> by caring for our planet, we can create a better world for future generations of Frenchie Pitbulls and their families.</p>
         </div>
         <span className='bg-gradient-to-br from-amber-300 via-purple-200 to-fuchsia-500 bg-clip-text text-transparent'></span>
     </div>
