b2d5ae95468239f5e99447c29597ca25602fa292
ajv55 <118780510+ajv55@users.noreply.github.com>
2024-04-04 13:50:17 -0400

    updating the different pages for responsiveness

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -45,7 +45,7 @@ export default function Nav(prop: HamburgerProps) {
       <HiMenuAlt1 size={40} className="md:hidden" color="white" onClick={handleMenu}/>
     </div>
     <AnimatePresence>
-      {isOpen && <motion.div initial={{scale: 0.2, opacity: 0.2, x: "-100%",}} animate={{x: '60%', scaleX: 0.3, rotate: 180}} transition={{duration: 1, ease: 'backOut'}}  whileInView={{scale: 1, opacity: 1, x: '0%', scaleX: 1, rotate: 0}} exit={{scale: 0.2, rotate: 180, opacity: 0.2, x: '-100%', scaleX: 0.8}} className=" w-[22rem]  h-screen rounded-xl fixed bg-gradient-to-br from-slate-500 via-slate-700 to-slate-900 backdrop-blur-xl top-0 left-0 -z-0 flex flex-col  justify-center items-end">
+      {isOpen && <motion.div initial={{scale: 0.2, opacity: 0.2, x: "-100%",}} animate={{x: '60%', scaleX: 0.3, rotate: 180}} transition={{duration: 1, ease: 'backOut'}}  whileInView={{scale: 1, opacity: 1, x: '0%', scaleX: 1, rotate: 0}} exit={{scale: 0.2, rotate: 180, opacity: 0.2, x: '-100%', scaleX: 0.8}} className=" w-[22rem]  h-screen rounded-xl fixed bg-gradient-to-br from-slate-500 via-slate-700 to-slate-900 backdrop-blur-xl top-0 left-0 z-0 flex flex-col  justify-center items-end">
             <div className=" w-full flex relative justify-center items-center p-6">
             <Image src={Logo}  alt="pup" style={{width: '100px', height: '100px'}}></Image>
             <GoX className="absolute top-0 right-0" size={43} color="white" onClick={handleMenu}/>
